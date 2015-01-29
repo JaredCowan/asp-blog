cd142c68984bebe9cb7da48a4673aec7f3bd224e
JaredCowan <jaredlc@outlook.com>
2015-01-28 18:43:18 -0800

    testing content return for controller

--- simpleblog/Controllers/PostsController.cs
+++ simpleblog/Controllers/PostsController.cs
@@ -10,7 +10,7 @@ namespace simpleblog.Controllers
     {
         public ActionResult Index()
         {
-            return Content("Hello, world!");
+            return Content("Thriii By Verde Circle");
         }
     }
 }
