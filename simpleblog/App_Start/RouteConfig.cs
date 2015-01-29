388a566271d27967ee2375e11be77e07360b9f87
JaredCowan <jaredlc@outlook.com>
2015-01-28 19:20:00 -0800

    frontend namespaces for routes fixed

--- simpleblog/App_Start/RouteConfig.cs
+++ simpleblog/App_Start/RouteConfig.cs
@@ -12,13 +12,13 @@ namespace simpleblog
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            var namespaces = new[] { typeof(PostsController) };
+            var namespaces = new[] { typeof(PostsController).Namespace };
 
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute("Login", "login", new { controller = "Auth", action = "Login" });
+            routes.MapRoute("Login", "login", new { controller = "Auth", action = "Login" }, namespaces);
 
-            routes.MapRoute("Home", "", new { controller = "Posts", action = "Index" });  
+            routes.MapRoute("Home", "", new { controller = "Posts", action = "Index" }, namespaces);  
         }
     }
 }
