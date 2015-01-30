3113784308b42a1cdedfe652338b301a7e59a732
JaredCowan <jaredlc@outlook.com>
2015-01-29 20:01:01 -0800

    added scripts and style packages

--- simpleblog/Global.asax.cs
+++ simpleblog/Global.asax.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
+using simpleblog.App_Start;
 
 namespace simpleblog
 {
@@ -13,6 +15,7 @@ namespace simpleblog
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
