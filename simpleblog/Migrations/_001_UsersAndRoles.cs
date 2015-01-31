9db5a88a466c5d3a45fa089af80655b94fa09cfa
JaredCowan <jaredlc@outlook.com>
2015-01-30 19:06:33 -0800

    added basic users database

---
+++ simpleblog/Migrations/_001_UsersAndRoles.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FluentMigrator;
+
+namespace simpleblog.Migrations
+{
+    [Migration(1)]
+    public class _001_UsersAndRoles : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("users")
+                .WithColumn("id").AsInt32().Identity().PrimaryKey()
+                .WithColumn("username").AsString(128)
+                .WithColumn("email").AsString(256)
+                .WithColumn("password_hash").AsString(128);
+        }
+
+        public override void Down()
+        {
+            Delete.Table("users");
+        }
+    }
+}
