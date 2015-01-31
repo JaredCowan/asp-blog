2fb7341c950a3c7407ddc1fe710a8daaf206d4b2
JaredCowan <jaredlc@outlook.com>
2015-01-30 20:44:34 -0800

    added bat file for migrating db

--- simpleblog/Migrations/_001_UsersAndRoles.cs
+++ simpleblog/Migrations/_001_UsersAndRoles.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using FluentMigrator;
@@ -14,13 +15,24 @@ namespace simpleblog.Migrations
             Create.Table("users")
                 .WithColumn("id").AsInt32().Identity().PrimaryKey()
                 .WithColumn("username").AsString(128)
-                .WithColumn("email").AsString(256)
+                .WithColumn("email").AsCustom("VARCHAR(256)")
                 .WithColumn("password_hash").AsString(128);
+
+            Create.Table("rolls")
+                .WithColumn("Id").AsInt32().Identity().PrimaryKey()
+                .WithColumn("name").AsString(128);
+
+            Create.Table("roll_users")
+                .WithColumn("user_id").AsInt32().ForeignKey("users", "id").OnDelete(Rule.Cascade)
+                .WithColumn("roll_id").AsInt32().ForeignKey("rolls", "id").OnDelete(Rule.Cascade);
         }
 
         public override void Down()
         {
+            Delete.Table("role_users");
+            Delete.Table("rolls");
             Delete.Table("users");
+            
         }
     }
 }
