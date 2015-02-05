19990cd7fa01933241b99167eaa931ac77b4b259
JaredCowan <jaredlc@outlook.com>
2015-02-04 16:52:59 -0800

    added users class and database mappings

--- simpleblog/Database.cs
+++ simpleblog/Database.cs
@@ -2,10 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using NHibernate.Cfg;
+using NHibernate.Mapping.ByCode;
+using simpleblog.Models;
 
 namespace simpleblog
 {
-    public class Database
+    public static class Database
     {
+        public static void Configure()
+        {
+            var config = new Configuration();
+
+            // Configure connection string
+            config.Configure();
+
+            // Add our mappings
+            var mapper = new ModelMapper();
+            mapper.AddMapping<UserMap>();
+
+            config.AddMapping(mapper.CompileMappingForAllExplicitlyAddedEntities());
+
+            // Create session factory
+
+        }
+
+        public static void OpenSession()
+        {
+        }
+
+        public static void CloseSession()
+        {
+        }
     }
 }
