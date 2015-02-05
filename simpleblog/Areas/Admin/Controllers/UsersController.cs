179ebe549b939a0c5dcccb94b434e143e656786c
JaredCowan <jaredlc@outlook.com>
2015-02-04 18:04:37 -0800

    db connected and showing data

--- simpleblog/Areas/Admin/Controllers/UsersController.cs
+++ simpleblog/Areas/Admin/Controllers/UsersController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NHibernate.Linq;
+using simpleblog.Areas.Admin.ViewModels;
 using simpleblog.Infrastructure;
+using simpleblog.Models;
 
 namespace simpleblog.Areas.Admin.Controllers
 {
@@ -13,7 +16,10 @@ namespace simpleblog.Areas.Admin.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View(new UsersIndex
+            {
+                Users = Database.Session.Query<User>().ToList()
+            });
         }
     }
 }
