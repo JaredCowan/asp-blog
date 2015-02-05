71a2f33f7e087aa8c1d109175e060213e838d43e
JaredCowan <jaredlc@outlook.com>
2015-02-04 17:05:43 -0800

    configured db extraction sesion

--- simpleblog/Database.cs
+++ simpleblog/Database.cs
@@ -11,7 +11,15 @@ namespace simpleblog
 {
     public static class Database
     {
+        private const string SessionKey = "simpleblog.Database.SessionKey";
+
         private static ISessionFactory _sessionFactory;
+
+        public static ISession Session
+        {
+            get { return (ISession) HttpContext.Current.Items[SessionKey]; }
+        }
+
         public static void Configure()
         {
             var config = new Configuration();
@@ -31,10 +39,16 @@ namespace simpleblog
 
         public static void OpenSession()
         {
+            HttpContext.Current.Items[SessionKey] = _sessionFactory.OpenSession();
         }
 
         public static void CloseSession()
         {
+            var session = HttpContext.Current.Items[SessionKey] as ISession;
+            if (session != null)
+                session.Close();
+
+            HttpContext.Current.Items.Remove(SessionKey);
         }
     }
 }
