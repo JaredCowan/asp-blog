3efe285938f1bd17747584c9bdeaa0f11b4f7e8a
JaredCowan <jaredlc@outlook.com>
2015-02-10 13:53:09 -0800

    changed postcontroller to show pageddata for post

--- simpleblog/Areas/Admin/Controllers/PostsController.cs
+++ simpleblog/Areas/Admin/Controllers/PostsController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NHibernate.Linq;
+using simpleblog.Areas.Admin.ViewModels;
 using simpleblog.Infrastructure;
+using simpleblog.Models;
 
 namespace simpleblog.Areas.Admin.Controllers
 {
@@ -11,9 +14,22 @@ namespace simpleblog.Areas.Admin.Controllers
     [SelectedTab("posts")]
     public class PostsController : Controller
     {
-        public ActionResult Index()
+        private const int PostPerPage = 5;
+
+        public ActionResult Index(int page = 1)
         {
-            return View();
+            var totalPostCount = Database.Session.Query<Post>().Count();
+
+            var currentPostPage = Database.Session.Query<Post>()
+                .OrderByDescending(c => c.CreatedAt)
+                .Skip((page - 1) * PostPerPage)
+                .Take(PostPerPage)
+                .ToList();
+
+            return View(new PostsIndex
+            {
+                Posts = new PagedData<Post>(currentPostPage, totalPostCount, page, PostPerPage)
+            });
         }
     }
 }
