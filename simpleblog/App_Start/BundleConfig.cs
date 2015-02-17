28ee43e4c21dc8a39754f4fd6d132c3552fe6b96
JaredCowan <jaredlc@outlook.com>
2015-02-16 20:36:35 -0800

    testing fix 9

--- simpleblog/App_Start/BundleConfig.cs
+++ simpleblog/App_Start/BundleConfig.cs
@@ -27,7 +27,9 @@ namespace simpleblog.App_Start
                 .Include("~/content/scripts/jquery-2.1.3.js")
                 .Include("~/content/scripts/jquery.validate.js")
                 .Include("~/content/scripts/jquery.validate.unobtrusive.js")
-                .Include("~/content/scripts/bootstrap.js"));
+                .Include("~/content/scripts/bootstrap.js")
+                .Include("~/content/scripts/fancybox.js")
+                .Include("~/content/scripts/common-scripts.js"));
 
             bundles.Add(new ScriptBundle("~/admin/post/scripts")
                 .Include("~/content/scripts/AdminPosts.js"));
